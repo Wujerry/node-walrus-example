fc3d5438c327893bd91132386a542a72e1337a95
Jerry Wu <wujerry116@163.com>
2024-12-03 16:25:00 +0800

    feat: initialize WalrusClient and log npx version

--- app/walrus.ts
+++ app/walrus.ts
@@ -1,20 +1,12 @@
 import { WalrusClient } from 'node-walrus-vercel'
 import path from 'path'
-import fs from 'fs'
 
 const clientConfig = path.resolve(process.cwd(), 'config', 'client_config.yaml')
 const walletConfig = path.resolve(process.cwd(), 'config', 'sui_config.yaml')
-console.log(clientConfig)
-console.log(walletConfig)
 
-// read file sync
-const clientConfigContent = fs.readFileSync(clientConfig, 'utf8')
-console.log(clientConfigContent)
-const walletConfigContent = fs.readFileSync(walletConfig, 'utf8')
-console.log(walletConfigContent)
-console.log(process.cwd(), 'process.cwd()')
-console.log(__dirname, '__dirname')
 const client = new WalrusClient(clientConfig, walletConfig)
+console.log('client inited')
+console.log(client.runNpxVersion(), 'client.runNpxVersion()')
 
 export function getInfo() {
   return client.getInfo()
