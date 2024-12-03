336b6eb90c6ecbd1867aff9539ad368dbfa05e11
Jerry Wu <wujerry116@163.com>
2024-12-03 20:05:30 +0800

    feat: read keystore file and log its content during Walrus client initialization

--- app/walrus.ts
+++ app/walrus.ts
@@ -1,12 +1,18 @@
 import { WalrusClient } from 'node-walrus'
 import path from 'path'
 import { downloadBin } from './util'
+import fs from 'fs'
 
 // donwload walrus binary to /tmp/node-walrus/bin when deloy on vercel
 await downloadBin()
 
 const clientConfig = path.resolve(process.cwd(), 'config', 'client_config.yaml')
 const walletConfig = path.resolve(process.cwd(), 'config', 'sui_config.yaml')
+const keyStore = path.resolve(process.cwd(), 'config', 'sui.keystore')
+console.log('clientConfig', keyStore)
+// read file form keyStore
+const keyStoreContent = await fs.promises.readFile(keyStore, 'utf-8')
+console.log('keyStoreContent', keyStoreContent)
 
 const client = new WalrusClient(clientConfig, walletConfig)
 console.log('client inited')
